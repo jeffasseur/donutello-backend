d8b03bbcbc5f7d8fdb27d583a0a23414ba985d4d
JEF .FASSEUR <91196774+jeffasseur@users.noreply.github.com>
2022-11-04 14:53:57 +0100

    feat: add structure in in routes in Donuts.js file

--- routes/api/v1/Donuts.js
+++ routes/api/v1/Donuts.js
@@ -1,27 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET ROUTES */
 /* GET routes for Donuts. */
-router.get('/', (req, res, next) => {
-  res.send('GET root donut route 🍩');
-});
+    router.get('/', (req, res, next) => {
+    res.send('GET root donut route 🍩');
+    });
 
-router.get('/:id', (req, res) => {
-    res.send(`GET donut route with id of ${req.params.id} 🍩`);
-});
+    router.get('/:id', (req, res) => {
+        res.send(`GET donut route with id of ${req.params.id} 🍩`);
+    });
 
-router.post('/', (req, res) => {
-    res.send('POST your homemade donut 🍩');
-});
+/* POST ROUTES */
+    router.post('/', (req, res) => {
+        res.send('POST your homemade donut 🍩');
+    });
 
-router.delete('/:id', (req, res) => {
-    res.send('DELETE donut with id: ' + req.params.id + '🍩');
-});
+/* PUT ROUTES */
+    router.put('/:id', (req, res) => {
+        res.send('UPDATE donut with id: ' + req.params.id + '🍩');
+    });
 
-router.put('/:id', (req, res) => {
-    res.send('UPDATE donut with id: ' + req.params.id + '🍩');
-});
-
+/* DELETE ROUTES */
+    router.delete('/:id', (req, res) => {
+        res.send('DELETE donut with id: ' + req.params.id + '🍩');
+    });
 
 
 module.exports = router;
