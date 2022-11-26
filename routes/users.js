1819364fabc9039f248a9ecc41ce18447dbbbfb9
JEF .FASSEUR <91196774+jeffasseur@users.noreply.github.com>
2022-11-26 16:07:02 +0100

    feat: add login route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// POST route for login
+router.post('/login', function(req, res, next) {
+  res.send('Login route');
+});
+
 module.exports = router;
