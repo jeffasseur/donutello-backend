8bcc66ce57177cca2f8aeb46ac91d18ce4f2eb8f
Sienvdb <sienvdbergh@gmail.com>
2022-11-29 23:38:23 +0100

    feat: add required fields to donut model

--- models/Donut.js
+++ models/Donut.js
@@ -3,11 +3,11 @@ const Schema = mongoose.Schema;
 
 const DonutSchema = new mongoose.Schema({
     id: Number,
-    name: String,
+    name: {type: String, required: true},
     description: String,
-    dough: String,
-    glase: String,
-    sprinkles: String,
+    dough: {type: String, required: true},
+    glase: {type: String, required: true},
+    sprinkles: {type: String, required: true},
     logo: String,
 });
 
