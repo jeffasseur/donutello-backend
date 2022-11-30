855fd2c04d545aa1e141b140e54bc6e73d4cee81
Sienvdb <sienvdbergh@gmail.com>
2022-11-30 12:13:18 +0100

    feat: add extra fields to donut model (make clear if it is order)

--- models/Donut.js
+++ models/Donut.js
@@ -3,12 +3,27 @@ const Schema = mongoose.Schema;
 
 const DonutSchema = new mongoose.Schema({
     id: Number,
+
+    //donut configuration
     name: {type: String, required: true},
-    description: String,
     dough: {type: String, required: true},
     glase: {type: String, required: true},
     sprinkles: {type: String, required: true},
     logo: String,
+    dateOfCreation: {type: Date, default: Date.now},
+    email: {type: String, required: true},
+
+    //check if order or game
+    order: {type: String, required: true, default: false},
+
+    //check if it is custom or random donut
+    type: {type: String, required: true},
+
+    //donuts for clients
+    client: {type: String},
+    amount: {type: Number, default: 0},
+    description: {type: String},
+
 });
 
 const Donut = mongoose.model('Donut', DonutSchema);
