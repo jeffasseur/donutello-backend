0bb1a98b38ba3cf06ef0526cf2738ac03d0caa24
Sienvdb <sienvdbergh@gmail.com>
2022-12-07 10:16:14 +0100

    feat: add login using bcrypt

--- controllers/api/v1/users.js
+++ controllers/api/v1/users.js
@@ -1,6 +1,7 @@
 const User = require('../../../models/User');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
+const bcrypt = require('bcrypt');
 
 
 const getAll = (req, res) => {
@@ -16,6 +17,12 @@ const signup = async (req, res) => {
 
     user.username = req.body.username;
     user.password = req.body.password;
+
+    //generate salt to hash password
+    const salt = await bcrypt.genSalt(10);
+
+    //set user password to hashed password
+    user.password = await bcrypt.hash(user.password, salt);    
 
     user.save().then(result => {
         let token = jwt.sign({
@@ -41,9 +48,47 @@ const signup = async (req, res) => {
 
 }
 
+const login = async (req, res) => {
+    const body = req.body;
+    const user = await User.findOne({username: body.username});
+    if(user) {
+        //check if user password and hashed password are the same
+        const validatePassword = await bcrypt.compare(body.password, user.password);
+
+        console.log(body.password);
+        console.log(user.password);
+        console.log(validatePassword);
+        if(validatePassword) {
+            let token = jwt.sign({
+                //uid: user._id, 
+                username: user.username,
+            }, "SecretWord");
+        
+        
+            res.json({
+                status: "success",
+                token: token
+            });
+        } else {
+
+            res.json({
+                status: "error",
+                message: "Password is incorrect"
+            });
+        }
+    } else {
+
+        res.json({
+            status: "error",
+            message: "No user found with this email"
+        });
+    }
+}
+
 // export the functions
 module.exports = {
     getAll,
     signup,
-    getByUsername
+    getByUsername,
+    login
 }
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 // import user controller
-const userController = require('../../../controllers/api/v1/users');
+const userController = require('../controllers/api/v1/users');
 
 /* GET users listing. */
 router.get('/', userController.getAll);
@@ -9,5 +9,6 @@ router.get('/username/username', userController.getByUsername);
 
 // POST route for login
 router.post('/signup', userController.signup);
+router.post('/login', userController.login);
 
 module.exports = router;
